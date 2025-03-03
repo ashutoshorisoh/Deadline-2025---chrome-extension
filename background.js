15fdf78637e1413f6085c678a83cb16d9a8d7296
Ashutosh <ashutoshorisoh@gmail.com>
2025-03-03 15:26:01 +0530

    fixed

--- background.js
+++ background.js
@@ -8,7 +8,6 @@ function updateTime() {
 
    const now = new Date();
 
-   // Countdown Timer (31 Dec 2025 - Current Time)
    const targetDate = new Date("December 31, 2025 23:59:59").getTime();
    const difference = targetDate - now.getTime();
 
@@ -23,10 +22,6 @@ function updateTime() {
    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
    timeElement.innerText = `${days}d ${hours}h ${minutes}m ${seconds}s`;
-
-   // Show Current Day (e.g., "Monday")
-   const options = { weekday: "long" };
-   dateElement.innerText = now.toLocaleString(undefined, options);
 }
 
 updateTime();
